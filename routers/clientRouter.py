bb39eac2ade43c3e52b22d1bcc1aaa075a77d29c
HenryTech12 <fakorodehenry@gmail.com>
2026-01-06 04:29:53 +0100

    fixed authorization issues

--- routers/clientRouter.py
+++ routers/clientRouter.py
@@ -6,16 +6,17 @@ from repository import database
 from schema import clientSchema
 from security import securityConfig
 from service import clientService
+from fastapi.security import HTTPAuthorizationCredentials
 client_router = APIRouter()
 
 @client_router.post('/clients/profile')
-def create_client_profile(data: clientSchema.ClientProfileSchema, email: str = Depends(securityConfig.get_current_user), db: Session = Depends(database.get_db)):
+def create_client_profile(data: clientSchema.ClientProfileSchema, email: HTTPAuthorizationCredentials = Depends(securityConfig.get_current_user), db: Session = Depends(database.get_db)):
     return clientService.create_client_profile(data,email,db)
 
 @client_router.get('/clients/profile')
-def fetch_client_profile(email: str = Depends(securityConfig.get_current_user), db: Session = Depends(database.get_db)):
+def fetch_client_profile(email: HTTPAuthorizationCredentials = Depends(securityConfig.get_current_user), db: Session = Depends(database.get_db)):
     return clientService.get_client_profile(email,db)
 
 @client_router.patch('/clients/profile')
-def update_client_profile(data: clientSchema.UpdateClientProfileSchema, email: str = Depends(securityConfig.get_current_user), db: Session = Depends(database.get_db)):
+def update_client_profile(data: clientSchema.UpdateClientProfileSchema,email: HTTPAuthorizationCredentials = Depends(securityConfig.get_current_user), db: Session = Depends(database.get_db)):
     return clientService.update_client_profile(email,data,db)
